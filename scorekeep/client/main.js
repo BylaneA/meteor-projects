39b6c6f3d3735081d4a3ce48fd6bd672b555484e
Bilane A <billieabdiw@gmail.com>
2017-12-14 16:20:29 +0100

    main js changed

--- scorekeep/client/main.js
+++ scorekeep/client/main.js
@@ -6,40 +6,8 @@ import {Tracker} from 'meteor/tracker';
 import {Players} from './../imports/api/players';
 import App from './../imports/ui/App';
 
-<<<<<<< HEAD
 //getting initial data
 //rendering app
-=======
-
-//transfomer l'objet array en jsx array
-const renderPlayers = (playersList) => {
-  return playersList.map((player) => {
-    return (
-      <p key={player._id}>
-        {player.name} has {player.score} point(s).
-        <button onClick={() => Players.update (player._id, {$inc: {score:1}})}>+1</button>
-        <button onClick={() => Players.update (player._id, {$inc: {score:-1}})}>-1</button>
-        <button onClick={() => Players.remove(player._id)}>X</button>
-      </p>
-    );
-  });
-};
-
-const handleSubmit = (e) => {
-  e.preventDefault();
-  let playerName = e.target.playerName.value;
-
-  if(playerName){
-    e.target.playerName.value='';
-    //players insert 
-    Players.insert({
-      name: playerName,
-      score: 0
-    });
-  };
-};
-
->>>>>>> 23e4c2a97d7291421139ddbe87f5006e5d39299b
 Meteor.startup(() => {
   Tracker.autorun(() => {
     let players = Players.find().fetch();
