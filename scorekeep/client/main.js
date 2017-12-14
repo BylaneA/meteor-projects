533d9c4f604f6acc46fc86ed308992b634a05961
Bilane A <billieabdiw@gmail.com>
2017-12-14 16:16:18 +0100

    update

--- scorekeep/client/main.js
+++ scorekeep/client/main.js
@@ -4,57 +4,14 @@ import {Meteor} from 'meteor/meteor';
 import {Tracker} from 'meteor/tracker';
 
 import {Players} from './../imports/api/players';
+import App from './../imports/ui/App';
 
-
-//transfomer l'objet array en jsx array
-const renderPlayers = (playersList) => {
-  return playersList.map((player) => {
-    return (
-      <p key={player._id}>
-        {player.name} has {player.score} point(s).
-        <button onClick={() => {
-          alert('deleted')
-        }}>X</button>
-      </p>
-    );
-  });
-};
-
-const handleSubmit = (e) => {
-  e.preventDefault();
-  let playerName = e.target.playerName.value;
-
-  if(playerName){
-    e.target.playerName.value='';
-    //players insert 
-    Players.insert({
-      name: playerName,
-      score: 0
-    });
-  };
-};
-
+//getting initial data
+//rendering app
 Meteor.startup(() => {
-
-  //allows you to run a function that depends on reactive data sources, in such a way that if there are changes to the data later, the function will be rerun
   Tracker.autorun(() => {
     let players = Players.find().fetch();
     let title = 'Score Keep';
-    let name = 'Mike';
-    let jsx = (
-      <div>
-        <h1>{title}</h1>
-        <p>Hello {name}!</p>
-        <p>This is my second p.</p>
-        {renderPlayers(players)}
-        <form onSubmit={handleSubmit}>
-          <input type="text" name="playerName" placeholder="Player name"/>
-          <button>Add Player</button>
-        </form>
-      </div>
-    );
-  ReactDOM.render(jsx, document.getElementById('app'));
+    ReactDOM.render(<App title= {title} players={players}/>, document.getElementById('app'));
   });
-
-
 });
