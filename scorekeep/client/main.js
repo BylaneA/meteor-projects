52b14654289ed7c80234d7450cf0d7baf3731aa0
BylaneA <billieabdiw@gmail.com>
2017-12-21 20:31:21 +0100

    sorting donne

--- scorekeep/client/main.js
+++ scorekeep/client/main.js
@@ -4,57 +4,12 @@ import {Meteor} from 'meteor/meteor';
 import {Tracker} from 'meteor/tracker';
 
 import {Players} from './../imports/api/players';
-
-
-//transfomer l'objet array en jsx array
-const renderPlayers = (playersList) => {
-  return playersList.map((player) => {
-    return (
-      <p key={player._id}>
-        {player.name} has {player.score} point(s).
-        <button onClick={() => Players.update (player._id, {$inc: {score:1}})}>+1</button>
-        <button onClick={() => Players.update (player._id, {$inc: {score:-1}})}>-1</button>
-        <button onClick={() => Players.remove(player._id)}>X</button>
-      </p>
-    );
-  });
-};
-
-const handleSubmit = (e) => {
-  e.preventDefault();
-  let playerName = e.target.playerName.value;
-
-  if(playerName){
-    e.target.playerName.value='';
-    //players insert 
-    Players.insert({
-      name: playerName,
-      score: 0
-    });
-  };
-};
+import App from './../imports/ui/App';
 
 Meteor.startup(() => {
-
-  //allows you to run a function that depends on reactive data sources, in such a way that if there are changes to the data later, the function will be rerun
   Tracker.autorun(() => {
-    let players = Players.find().fetch();
+    let players = Players.find({}, {sort: {score: -1}}).fetch();
     let title = 'Score Keep';
-    let name = 'Mike';
-    let jsx = (
-      <div>
-        <h1>{title}</h1>
-        <p>Hello {name}!</p>
-        <p>This is my second p.</p>
-        {renderPlayers(players)}
-        <form onSubmit={handleSubmit}>
-          <input type="text" name="playerName" placeholder="Player name"/>
-          <button>Add Player</button>
-        </form>
-      </div>
-    );
-  ReactDOM.render(jsx, document.getElementById('app'));
+    ReactDOM.render(<App title={title} players={players}/>, document.getElementById('app'));
   });
-
-
 });
---
+++ scorekeep/imports/ui/TitleBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default class TitleBar extends React.Component {
+  renderSubtitle() {
+    if (this.props.subtitle) {
+      return <h2>{this.props.subtitle}</h2>;
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.props.title}</h1>
+        {this.renderSubtitle()}
+      </div>
+    );
+  }
+}
+
